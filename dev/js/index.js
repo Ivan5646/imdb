518d890d98feacc99ded372b7d549efadbaaf229
Ivan5646 <Mike000027@yandex.ru>
2018-11-28 20:15:39 +0700

    installed redux-logger

--- dev/js/index.js
+++ dev/js/index.js
@@ -4,10 +4,15 @@ import ReactDOM from "react-dom"; // all above imported from node modules
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from "redux"; // {} means import a var or function from package
 import thunk from "redux-thunk";
+import  createLogger  from 'redux-logger'
 import allReducers from "./reducers";
 import App from './components/app';
 
-const store = createStore(allReducers, applyMiddleware(thunk));
+const logger = createLogger({
+    //empty options
+});
+
+const store = createStore(allReducers, applyMiddleware(thunk, logger));
 
 // here <Provider store={store}>  every component has access to store data now  
 ReactDOM.render(
