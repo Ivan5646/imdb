bc768eddd65ec9d59561a1a4d89e8152c83f9fb5
Ivan5646 <Mike000027@yandex.ru>
2018-12-26 00:16:45 +0700

    favourites reducer data received

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from "redux";
 import popularReducer from "./popularReducer";
 import searchReducer from "./searchReducer";
 import movieReducer from "./movieReducer";
-import favouritesReducer from "./movieReducer";
+import favouritesReducer from "./favouritesReducer";
 
 const allReducers = combineReducers({ // that will be thrown to the Store
     popularMovies: popularReducer,
