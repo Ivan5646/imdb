098794b9e1c4d6724d8d9d8dcbfacf3f4119cd5a
Ivan5646 <Mike000027@yandex.ru>
2018-12-27 00:43:54 +0700

    persist to LS started

--- src/index.js
+++ src/index.js
@@ -6,17 +6,26 @@ import thunk from 'redux-thunk'
 import { logger } from 'redux-logger'
 import reducer from './reducers'
 import './css/styles.scss'
+import { loadState, saveState } from "./persistState/localStorage";
 import App from "./components/App"
 
-
+const persistedState = loadState();
 const store = createStore(
     reducer,
+    persistedState,
     applyMiddleware(thunk, logger)
-)
+);
 
 render(
     <Provider store={store}>
         <App />
     </Provider>,
     document.getElementById('root')
-)
+);
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
+
+const myStore = store.getState();
+console.log("myStore", myStore.favourites);
